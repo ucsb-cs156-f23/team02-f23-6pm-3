579bde648eee63328cdfd9e40aabb8bd1ddbadfb
Yicong Yan <yicongyan@ucsb.edu>
2023-10-26 15:16:08 -0700

    add controller and not finished test

--- src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationController.java
@@ -1,8 +1,8 @@
 package edu.ucsb.cs156.example.controllers;
 
-import edu.ucsb.cs156.example.entities.UCSBDiningCommons;
+import edu.ucsb.cs156.example.entities.UCSBOrganization;
 import edu.ucsb.cs156.example.errors.EntityNotFoundException;
-import edu.ucsb.cs156.example.repositories.UCSBDiningCommonsRepository;
+import edu.ucsb.cs156.example.repositories.UCSBOrganizationRepository;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -22,94 +22,44 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@Tag(name = "UCSBDiningCommons")
-@RequestMapping("/api/ucsbdiningcommons")
+@Tag(name = "UCSBOrganization")
+@RequestMapping("/api/ucsborganization")
 @RestController
 @Slf4j
-public class UCSBDiningCommonsController extends ApiController {
+public class UCSBOrganizationController extends ApiController {
 
     @Autowired
-    UCSBDiningCommonsRepository ucsbDiningCommonsRepository;
+    UCSBOrganizationRepository ucsbOrganizationRepository;
 
-    @Operation(summary= "List all ucsb dining commons")
+    @Operation(summary= "List all ucsb organization")
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/all")
-    public Iterable<UCSBDiningCommons> allCommonss() {
-        Iterable<UCSBDiningCommons> commons = ucsbDiningCommonsRepository.findAll();
-        return commons;
+    public Iterable<UCSBOrganization> allOrganizations() {
+        Iterable<UCSBOrganization> organization = ucsbOrganizationRepository.findAll();
+        return organization;
     }
 
-    @Operation(summary= "Create a new commons")
+    @Operation(summary= "Create a new organization")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/post")
-    public UCSBDiningCommons postCommons(
-        @Parameter(name="code") @RequestParam String code,
-        @Parameter(name="name") @RequestParam String name,
-        @Parameter(name="hasSackMeal") @RequestParam boolean hasSackMeal,
-        @Parameter(name="hasTakeOutMeal") @RequestParam boolean hasTakeOutMeal,
-        @Parameter(name="hasDiningCam") @RequestParam boolean hasDiningCam,
-        @Parameter(name="latitude") @RequestParam double latitude,
-        @Parameter(name="longitude") @RequestParam double longitude
+    public UCSBOrganization postOrganization(
+        @Parameter(name="orgCode") @RequestParam String orgCode,
+        @Parameter(name="orgTranslationShort") @RequestParam String orgTranslationShort,
+        @Parameter(name="orgTranslation") @RequestParam String orgTranslation,
+        @Parameter(name="inactive") @RequestParam boolean inactive
         )
         {
 
-        UCSBDiningCommons commons = new UCSBDiningCommons();
-        commons.setCode(code);
-        commons.setName(name);
-        commons.setHasSackMeal(hasSackMeal);
-        commons.setHasTakeOutMeal(hasTakeOutMeal);
-        commons.setHasDiningCam(hasDiningCam);
-        commons.setLatitude(latitude);
-        commons.setLongitude(longitude);
+        UCSBOrganization organization = new UCSBOrganization();
+        organization.setOrgCode(orgCode);
+        organization.setOrgTranslationShort(orgTranslationShort);
+        organization.setOrgTranslation(orgTranslation);
+        organization.setInactive(inactive);
+        
 
-        UCSBDiningCommons savedCommons = ucsbDiningCommonsRepository.save(commons);
+        UCSBOrganization savedOrganization = ucsbOrganizationRepository.save(organization);
 
-        return savedCommons;
+        return savedOrganization;
     }
 
-    @Operation(summary= "Get a single commons")
-    @PreAuthorize("hasRole('ROLE_USER')")
-    @GetMapping("")
-    public UCSBDiningCommons getById(
-            @Parameter(name="code") @RequestParam String code) {
-        UCSBDiningCommons commons = ucsbDiningCommonsRepository.findById(code)
-                .orElseThrow(() -> new EntityNotFoundException(UCSBDiningCommons.class, code));
-
-        return commons;
-    }
-
-    @Operation(summary= "Delete a UCSBDiningCommons")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @DeleteMapping("")
-    public Object deleteCommons(
-            @Parameter(name="code") @RequestParam String code) {
-        UCSBDiningCommons commons = ucsbDiningCommonsRepository.findById(code)
-                .orElseThrow(() -> new EntityNotFoundException(UCSBDiningCommons.class, code));
-
-        ucsbDiningCommonsRepository.delete(commons);
-        return genericMessage("UCSBDiningCommons with id %s deleted".formatted(code));
-    }
-
-    @Operation(summary= "Update a single commons")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @PutMapping("")
-    public UCSBDiningCommons updateCommons(
-            @Parameter(name="code") @RequestParam String code,
-            @RequestBody @Valid UCSBDiningCommons incoming) {
-
-        UCSBDiningCommons commons = ucsbDiningCommonsRepository.findById(code)
-                .orElseThrow(() -> new EntityNotFoundException(UCSBDiningCommons.class, code));
-
-
-        commons.setName(incoming.getName());  
-        commons.setHasSackMeal(incoming.getHasSackMeal());
-        commons.setHasTakeOutMeal(incoming.getHasTakeOutMeal());
-        commons.setHasDiningCam(incoming.getHasDiningCam());
-        commons.setLatitude(incoming.getLatitude());
-        commons.setLongitude(incoming.getLongitude());
-
-        ucsbDiningCommonsRepository.save(commons);
-
-        return commons;
-    }
 }
